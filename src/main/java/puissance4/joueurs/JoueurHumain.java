5e76968f823e21e211404395ff5cb871d566c17e
Lapierre JeanRémi <jeanremi.lapierre@gmail.com>
2022-12-04 18:09:29 +0100

    ajout d'un type de joueur : joueur humain

---
+++ src/main/java/puissance4/joueurs/JoueurHumain.java
@@ -0,0 +1,33 @@
+package puissance4.joueurs;
+
+import java.util.Scanner;
+
+import puissance4.grille.Grille;
+
+public class JoueurHumain extends Joueur{
+
+	/**
+	 * rien a rajouter au constructeur
+	 * @param pion
+	 */
+	public JoueurHumain(char pion) {
+		super(pion);
+	}
+
+	@Override
+	public int choix(Grille grille) {
+		System.out.println(grille); //pour que le joueur voie la grille
+		System.out.println("votre pion" + this.pion);
+	    Scanner input = new Scanner(System.in);
+	    String reponse="";
+	    while(!(reponse.equals("1") || reponse.equals("2") || reponse.equals("3") || 
+	    		reponse.equals("4") || reponse.equals("5") || reponse.equals("6") || 
+	    		reponse.equals("7"))) {
+	    	System.out.print("votre choix : ");
+		    reponse = input.nextLine();
+	    }
+	    input.close();
+		return Integer.parseInt(reponse);
+	}
+
+}
